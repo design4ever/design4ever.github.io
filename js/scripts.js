a3bd0a1b7405929a97bf4d6c2ffc3730fd22df41
Debasmita <debasmitamajumder0@gmail.com>
2018-12-21 12:28:56 +0530

    Back to top added

--- js/scripts.js
+++ js/scripts.js
@@ -46,7 +46,21 @@
 		}
 	
 	});
-	
+
+	$(document).ready(function(){ 
+		$(window).scroll(function(){ 
+			if ($(this).scrollTop() > 100) { 
+				$('#toTop').fadeIn(); 
+			} else { 
+				$('#toTop').fadeOut(); 
+			} 
+		}); 
+		$('#toTop').click(function(){ 
+			$("html, body").animate({ scrollTop: 0 }, 600); 
+			return false; 
+		}); 
+	});
+
 	
 	$('a[href="#"]').on('click', function(event){
 		return;
